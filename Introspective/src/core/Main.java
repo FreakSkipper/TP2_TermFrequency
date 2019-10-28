7d77cfb4b1d69738235a633459910c334a263d81
Eduardo F. Assis <eduardoffassis@gmail.com>
2019-10-28 19:29:37 -0300

    Applied Introspective paradigm.

---
+++ Introspective/src/core/Main.java
@@ -0,0 +1,113 @@
+package core;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.Vector;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+	private static Scanner user_input;			// destinado ao input do usuario (https://stackoverflow.com/q/13042008)
+	
+	
+	public static void CLEAR() {
+		System.out.println(new String(new char[50]).replace("\0", "\r\n"));
+	}
+	
+	
+	public static Vector<String> readDocument() {
+		// conferindo chamada de funcao
+		String className = new Exception().getStackTrace()[1].getMethodName();
+		String expectedCaller = "main";
+		if (className != expectedCaller) {
+			System.out.println("> Chamada invalida de funcao");
+			System.out.println("\tCaller: " +className + "\n\tExpected: " +expectedCaller);
+			return null;
+		}
+		
+		// abrindo arquivo
+		String nome_arquivo = "SomeText.txt";
+		File file = new File(System.getProperty("user.dir") + "/Introspective/src/" + nome_arquivo);
+		String str;
+		Vector<String> string_vector = new Vector<String>();
+		
+		try {
+			BufferedReader bufferedreader = new BufferedReader(new FileReader(file));
+			while ((str = bufferedreader.readLine()) != null) { // enquanto ocorrer uma leitura
+				string_vector.add(str);
+			}
+			bufferedreader.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("> Nao foi possivel ler o arquivo: " + nome_arquivo);
+		}
+		
+		return string_vector;
+	}
+
+	
+	public static int menu() {
+		int opt = -1;
+		
+		do {
+			// CLEAR();
+			System.out.println("############################ Term Frequency ############################");
+			System.out.println("> 1 - Calcular a frequencia de um termo");
+			System.out.println("> 2 - Sair");
+			System.out.print(">> ");
+			
+			String readString = user_input.nextLine();
+			opt = Integer.parseInt(readString);
+		} while(opt != 1 && opt != 2);
+				
+		return opt;
+	}
+	
+	
+	public static void main(String[] args) {
+		Vector<String> text = readDocument();
+		if (text == null) return;
+		
+		boolean stop = false;
+		user_input = new Scanner(System.in);
+		
+		do {
+			int opt = menu();
+			
+			if (opt == 1) {				
+				System.out.println("> Insira o termo a ser procurado:");
+				System.out.print(">> ");
+				
+				String termo = user_input.nextLine();
+				System.out.println(termo);
+				// caso o usuario ensira mais de 1 palavra, soh pega a 1
+				String[] first_word = termo.split(" ");		
+				
+				TermFrequency tf = new TermFrequency();			// frequencia do termo
+				tf.termFreq(text, first_word[0]);
+				// acessando atributo usando introspection
+				Field[] fields_tf = tf.getClass().getDeclaredFields();
+				
+				if (fields_tf.length > 0) {
+					Field field = fields_tf[0];
+					field.setAccessible(true);
+					try {
+						System.out.println("> A frequencia do termo " + "\"" +first_word[0] + "\"" + " eh " +field.get(tf) + ".");
+					} catch(Exception e) {
+						e.printStackTrace();
+					}
+				}
+				else {
+					System.out.println("> Erro ao acessar atributos de " +tf.getClass());
+				}
+			}
+		} while(stop);
+		
+		user_input.close();
+	}
+}
--- Introspective/src/core/SampleReflection.java
+++ Introspective/src/core/SampleReflection.java
@@ -18,16 +18,16 @@ public class SampleReflection {
 		return 2 + 2;
 	}
 	public static void main(String[] args) {
-		SampleReflection class_test = new SampleReflection();
+		SampleReflection object_test = new SampleReflection();
 		// String fmt = "%6S:  %-12s = %s%n";
 		
 		try {
-			Class<?> c = class_test.getClass();
+			Class<?> c = object_test.getClass();
 			
 			Field field_name = c.getDeclaredField("name");
-			System.out.println("Nome do campo: " + field_name.getName() + " | Valor: " + field_name.get(class_test));
-			field_name.set(class_test, "Eduardo eh lindo e gostoso!");
-			System.out.println("Nome do campo: " + field_name.getName() + " | Valor: " + field_name.get(class_test));
+			System.out.println("Nome do campo: " + field_name.getName() + " | Valor: " + field_name.get(object_test));
+			field_name.set(object_test, "Eduardo eh lindo e gostoso!");
+			System.out.println("Nome do campo: " + field_name.getName() + " | Valor: " + field_name.get(object_test));
 			
 			Method[] allMethods = c.getDeclaredMethods();
 			for(Method m : allMethods) {
@@ -35,7 +35,7 @@ public class SampleReflection {
 				
 				try {
 					if (m.getGenericReturnType() == int.class) {
-						System.out.println(m.invoke(class_test));
+						System.out.println(m.invoke(object_test));
 					}
 				// Handle any exceptions thrown by method to be invoked.
 				} catch (InvocationTargetException x) {
